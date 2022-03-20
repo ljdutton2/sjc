38cbcd04b6d1d02febf471fc3a03c83925865274
lauren <ljdutton2@icloud.com>
2022-03-19 20:33:42 -0500

    front end working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,77 @@
-
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import contract from './contracts/sjc.json';
+import contract from './contracts/sjcContract.json';
+import { ethers } from 'ethers';
 
 const contractAddress = "0xF1DDA9F1527322aE55F187E71f53a1743ee367Bc";
 const abi = contract.abi;
 
 function App() {
 
-  const checkWalletIsConnected = () => { }
+  const [currentAccount, setCurrentAccount] = useState(null);
 
-  const connectWalletHandler = () => { }
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
 
-  const mintNftHandler = () => { }
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet confirmed!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const mintNftHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const sjcContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        let sjcTxn = await sjcContract.mintNFTs(1, { value: ethers.utils.parseEther("0.01") });
+
+        console.log("Mining... please wait");
+        await sjcTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${sjcTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   const connectWalletButton = () => {
     return (
@@ -36,9 +95,9 @@ function App() {
 
   return (
     <div className='main-app'>
-      <h1>stoned jets club</h1>
+      <h1>Stoned Jets Club</h1>
       <div>
-        {connectWalletButton()}
+        {currentAccount ? mintNftButton() : connectWalletButton()}
       </div>
     </div>
   )
